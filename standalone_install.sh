a4da680acf91f8d1acfaaf47c7575fa236eb7865
Brian Hunter <brian@tesora.com>
2014-10-09 17:13:15 +0000

    WIP.. repeating the build process on master jenkins.

---
+++ standalone_install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sudo apt-get install git -y
+git clone https://github.com/therootcause/seabass.git
+cd seabass/
+
+exit
+    
+sudo apt-get install ruby 
+    8  sudo gem install bundler --no-ri --no-rdoc
+    9  bundle
+   10  sudo apt-get libgecode-dev
+   11  sudo apt-get install libgecode-dev
+   12  bundle
+   13  sudo apt-get install libgecode36 
+   14  sudo bundle
+   15  vi /var/lib/gems/1.9.1/gems/dep-selector-libgecode-1.0.2/ext/libgecode3/vendor/gecode-3.7.3/config.log 
+   16  sudo apt-get install install gcc
+   17  sudo apt-get install gcc
+   18  vi /var/lib/gems/1.9.1/gems/dep-selector-libgecode-1.0.2/ext/libgecode3/vendor/gecode-3.7.3/config.log 
+   19  g++
+   20  sudo apt-get install g++\
+   21  sudo apt-get install g++
+   22  sudo bundle
+   23  autoconf
+   24  sudo apt-get install build-essentials
+   25  sudo apt-get install buildessentials
+   26  sudo apt-get install autoconf
+   27  sudo bundle
+   28  berks
+   29  sid poberks
+   30  sudo berks
+   31  apt-get search ffi
+   32  apt-cache search ffi
+   33  apt-cache search libffi
+   34  sudo apt-get install libffi6
+   35  sudo apt-get install libffi-dev
+   36  berks
+   37  apt-get install bundler
+   38  sudo apt-get install bundler
+   39  bundle
+   40  bundle exec berks
+   41  berks
+   42  gem install berkshelf
+   43  vi Gemfile
+   44  bundle
+   45  berks 
+   46  history > file
